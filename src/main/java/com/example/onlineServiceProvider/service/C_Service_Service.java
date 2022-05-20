158fde167efccdc568d25dc9ade89392181e5f0a
mmd2 <m.mohammad.7521@gmail.com>
2022-05-20 09:15:26 +0430

    deleting redundant code and code cleanup

--- src/main/java/com/example/onlineServiceProvider/service/C_Service_Service.java
+++ src/main/java/com/example/onlineServiceProvider/service/C_Service_Service.java
@@ -2,13 +2,17 @@ package com.example.onlineServiceProvider.service;
 
 
 import com.example.onlineServiceProvider.entity.c_service.C_Service;
+import com.example.onlineServiceProvider.entity.user.Expert;
+
+import java.util.List;
 
 public interface C_Service_Service {
 
-    public void add(C_Service service);
+    void add(C_Service service);
 
-    public void remove(C_Service service);
+    void remove(C_Service service);
 
-    public void update(C_Service service);
+    void update(C_Service service);
 
+    List<Expert> serviceExperts(C_Service service);
 }
